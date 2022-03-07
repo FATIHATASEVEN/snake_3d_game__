0a19151eced2919519c8a063d76fd20fdc0a04f3
Fatih ATASEVEN <93430023+FATIHATASEVEN@users.noreply.github.com>
2022-03-07 20:19:38 +0300

    snake
    game

---
+++ SnakeGame/Assets/scripts/Borders.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class Borders : MonoBehaviour
+{
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.CompareTag("SnakeMain"))
+		{
+			Application.LoadLevel(Application.loadedLevel);			
+		}
+
+	}
+}
+
+
+
+
---
+++ SnakeGame/Assets/scripts/Food.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("SnakeMain"))
+		{
+			other.GetComponent<SnakeMovment>().AddTail();
+			Destroy(gameObject);
+		}
+	}
+
+}
